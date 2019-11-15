e40b33c53c7338f3cc570dfa76f34c8744ce43a7
Davit Margaryan <davitmargaryan1987@gmail.com>
2019-11-15 17:40:08 +0400

    Updates

---
+++ precache-manifest.b3f9acf991490d54880bb157ddd52f44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9ed0fafdc5df6dbe1811af6ce590818",
+    "url": "/react-gp/index.html"
+  },
+  {
+    "revision": "7f57a0980b723d1c5c86",
+    "url": "/react-gp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "dae1d4e0cfa97f9d8850",
+    "url": "/react-gp/static/js/2.b812a4bf.chunk.js"
+  },
+  {
+    "revision": "7f57a0980b723d1c5c86",
+    "url": "/react-gp/static/js/main.408ed45b.chunk.js"
+  },
+  {
+    "revision": "f9152f01d3c88daca9dc",
+    "url": "/react-gp/static/js/runtime-main.a7ce5471.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-gp/static/media/logo.25bf045c.svg"
+  }
+]);
